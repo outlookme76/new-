ef9f811a8fe34d1b4c6326b9b66bd40be6ebdfb8
shivam <outlookme76@gmail.com>
2026-02-11 13:54:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,81 @@
-const intro=document.getElementById("intro");
-const proposal=document.getElementById("proposal");
-const slideshow=document.getElementById("slideshow");
-const ringSection=document.getElementById("ringSection");
-const finalPage=document.getElementById("finalPage");
+const screens = document.querySelectorAll(".screen");
+const startBtn = document.getElementById("startBtn");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const proposalText = document.getElementById("proposalText");
+const slideImage = document.getElementById("slideImage");
+const slideCaption = document.getElementById("slideCaption");
+const music = document.getElementById("bgMusic");
+const muteBtn = document.getElementById("muteBtn");
 
-const nameInput=document.getElementById("nameInput");
-const startBtn=document.getElementById("startBtn");
-const proposalText=document.getElementById("proposalText");
-
-const yesBtn=document.getElementById("yesBtn");
-const noBtn=document.getElementById("noBtn");
-
-const slideImage=document.getElementById("slideImage");
-const slideCaption=document.getElementById("slideCaption");
-const progress=document.getElementById("progress");
-
-const finalMessage=document.getElementById("finalMessage");
-const music=document.getElementById("bgMusic");
-
-/* Floating Hearts */
-setInterval(()=>{
-  const heart=document.createElement("div");
-  heart.classList.add("heart");
-  heart.style.left=Math.random()*100+"vw";
-  heart.style.animationDuration=(3+Math.random()*3)+"s";
-  document.body.appendChild(heart);
-  setTimeout(()=>heart.remove(),6000);
-},300);
-
-/* Name */
-startBtn.onclick=()=>{
-  const name=nameInput.value || "My Love";
-  intro.classList.add("hidden");
-  proposal.classList.remove("hidden");
-  typeWriter(`Will you be my Valentine, ${name}? 💖`);
-};
-
-function typeWriter(text){
-  let i=0;
-  proposalText.innerHTML="";
-  function typing(){
-    if(i<text.length){
-      proposalText.innerHTML+=text.charAt(i);
-      i++;
-      setTimeout(typing,50);
-    }
-  }
-  typing();
+function showScreen(id){
+  screens.forEach(s => s.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
 }
 
-/* Real Photos */
-const photos=[
+/* Start */
+startBtn.onclick = () => {
+  const name = document.getElementById("nameInput").value || "My Love";
+  showScreen("proposal");
+  proposalText.innerText = `Will you be my Valentine, ${name}? 💖`;
+
+  music.volume = 0;
+  music.play();
+
+  let vol = 0;
+  const fade = setInterval(()=>{
+    if(vol < 1){
+      vol += 0.05;
+      music.volume = vol;
+    } else clearInterval(fade);
+  },200);
+};
+
+/* Mute */
+muteBtn.onclick = () => {
+  music.muted = !music.muted;
+  muteBtn.innerText = music.muted ? "🔇" : "🔊";
+};
+
+/* Playful No */
+let noCount = 0;
+noBtn.onmouseover = () => {
+  noCount++;
+  if(noCount === 1) noBtn.innerText = "Are you sure? 🥺";
+  else if(noCount === 2) noBtn.innerText = "Please? 🥹";
+  else if(noCount === 3) noBtn.style.display = "none";
+};
+
+/* Real Images */
+const photos = [
   {src:"images/1.jpg",caption:"Our beautiful beginning 💕"},
   {src:"images/2.jpg",caption:"Moments I cherish 🥰"},
   {src:"images/3.jpg",caption:"Your smile is my happiness 😍"},
   {src:"images/4.jpg",caption:"Every second with you matters 💖"},
   {src:"images/5.jpg",caption:"Forever feels right with you 💍"},
-  {src:"images/6.jpg",caption:"My favorite person in the world 🌎"},
+  {src:"images/6.jpg",caption:"My favorite person 🌎"},
   {src:"images/7.jpg",caption:"This is just the beginning ✨"}
 ];
 
-let index=0;
+let index = 0;
 
-yesBtn.onclick=()=>{
-  music.play();
-  proposal.classList.add("hidden");
-  slideshow.classList.remove("hidden");
+yesBtn.onclick = () => {
+  showScreen("slideshow");
   launchConfetti();
   showSlides();
 };
 
 function showSlides(){
-  if(index<photos.length){
-    slideImage.src=photos[index].src;
-    slideCaption.innerText=photos[index].caption;
-    progress.style.width=((index+1)/photos.length)*100+"%";
+  if(index < photos.length){
+    slideImage.src = photos[index].src;
+    slideCaption.innerText = photos[index].caption;
     index++;
     setTimeout(showSlides,2500);
-  }else{
-    slideshow.classList.add("hidden");
-    ringSection.classList.remove("hidden");
-    setTimeout(()=>{
-      ringSection.classList.add("hidden");
-      finalPage.classList.remove("hidden");
-      finalMessage.innerText="I promise to love you endlessly 💖";
-    },6000);
+  } else {
+    showScreen("ringSection");
+    setTimeout(()=> showScreen("finalPage"),6000);
   }
 }
-
-/* Playful No */
-let noCount=0;
-noBtn.addEventListener("mouseover",()=>{
-  noCount++;
-  if(noCount===1) noBtn.innerText="Are you sure? 🥺";
-  else if(noCount===2) noBtn.innerText="Please? 🥹";
-  else if(noCount===3) noBtn.style.display="none";
-  noBtn.style.position="absolute";
-  noBtn.style.left=Math.random()*80+"%";
-  noBtn.style.top=Math.random()*80+"%";
-  yesBtn.style.transform=`scale(${1+noCount*0.1})`;
-});
 
 /* Confetti */
 function launchConfetti(){
